ccac0c94fd42fd7a49b9348c1454d18d2ef4e254
Jason Groenjes <jason@groenjes.com>
2024-08-25 14:39:39 -0500

    Initial Commit

---
+++ Tests/KrowdKinect-Swift-SDKTests/KrowdKinect_Swift_SDKTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import KrowdKinect_Swift_SDK
+
+final class KrowdKinect_Swift_SDKTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
